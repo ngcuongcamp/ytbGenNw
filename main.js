687535abaeda136820c16f875097aa5d7295cbc0
nguyencuongzth <nguyencuongzth@gmail.com>
2025-05-19 09:18:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,108 @@
+const ytFrame = document.getElementById("yt_frame");
+const searchField = document.getElementById("search_field");
+const searchButton = document.getElementById("search_button");
+const defaultButton = document.getElementById("default_button");
+const saveDfButton = document.getElementById("save_df");
+const embedUrl = "https://www.youtube-nocookie.com/embed/"
+
+
+let videoID = "PaQX0pktLnw"
+let defaultVideoID = "PaQX0pktLnw"
+const handle_init = () => {
+    let { videoID, idDefault } = init_localStorage();
+
+    if (videoID) {
+        const videoUrl = `${embedUrl}` + `${videoID}` + "?controls=1&autoplay=1&loop=1"
+        ytFrame.src = videoUrl;
+    }
+
+    const WindowWidth = document.documentElement.clientWidth;
+    const WindowHeight = document.documentElement.clientHeight - 30 - 20;
+
+    ytFrame.style.width = `${WindowWidth}px`;
+    ytFrame.style.height = `${WindowHeight}px`;
+
+    defaultVideoID = idDefault
+};
+
+const handle_resize = () => {
+    const WindowWidth = document.documentElement.clientWidth;
+    const WindowHeight = document.documentElement.clientHeight - 30 - 20;
+
+    ytFrame.style.width = `${WindowWidth}px`;
+    ytFrame.style.height = `${WindowHeight}px`;
+}
+
+const init_localStorage = () => {
+    console.log('initialize')
+
+    let local_variable = window.localStorage.getItem('ngcuongzth_ytb');
+    local_variable ? localStorage.setItem('ngcuongzth_ytb', local_variable) : localStorage.setItem('ngcuongzth_ytb', videoID);
+    videoID = localStorage.getItem('ngcuongzth_ytb');
+
+
+    let idDefault = window.localStorage.getItem('ngcuongzth_ytb_df_id');
+    idDefault ? localStorage.setItem('ngcuongzth_ytb_df_id', idDefault) : localStorage.setItem('ngcuongzth_ytb_df_id', videoID);
+    return { videoID, idDefault }
+}
+
+
+const handle_save_df = () => {
+    const vdId = window.localStorage.getItem('ngcuongzth_ytb')
+    localStorage.setItem('ngcuongzth_ytb_df_id', vdId)
+    console.log('set to default ')
+}
+
+
+
+
+const handle_get_id_video = () => {
+    const searchValue = searchField.value;
+    let videoId = "";
+
+    if (searchValue.includes("genyt")) {
+        videoId = searchValue.split("/").pop()
+        console.log('videoId: ', videoId)
+    }
+    else if (searchValue.includes("www.youtube.com") && !searchValue.includes("watch")) {
+        // https://www.youtube.com/watch?v=VPvVD8t02U8
+        videoId = searchValue.split("?v=")[1];
+    }
+    else if (searchValue.includes("www.youtube.com/watch?")) {
+        videoId = searchValue.split("v=")[1].split("&")[0];
+
+    }
+    else {
+        alert("Wrong url format!");
+        return;
+    }
+
+
+    if (videoId === undefined) {
+        alert("Not found or link is incorrect!");
+    }
+    else {
+        window.localStorage.setItem('ngcuongzth_ytb', videoId);
+        const newUrl = embedUrl + videoId + "?controls=1&autoplay=1&loop=1";
+        ytFrame.src = newUrl;
+        handle_resize();
+    }
+
+
+}
+
+const handle_set_default = () => {
+    const urlBase = 'https://www.youtube.com/watch?v='
+    const url_default = urlBase + defaultVideoID
+    console.log(url_default);
+    searchField.value = url_default;
+    searchButton.click();
+}
+
+
+window.addEventListener("DOMContentLoaded", handle_init);
+window.addEventListener("resize", handle_resize);
+searchButton.addEventListener("click", handle_get_id_video)
+defaultButton.addEventListener("click", handle_set_default)
+saveDfButton.addEventListener("click", handle_save_df)
+
